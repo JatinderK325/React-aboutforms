22396d87c5c61ed697b7b65c305727b3fecb212b
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-07-23 13:59:59 -0400

    project-setup

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  // const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+    setEnteredName('');
+  }
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input
+          value={enteredName}
+          // ref={nameInputRef} 
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler} />
+          {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
